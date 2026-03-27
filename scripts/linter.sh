3b384adee4f7e0cefd7d6f7f435a8ab695dfebb9
X-HackerOne/X-Bug-Bounty <forgevertical@wearehackerone.com>
2026-03-27 15:52:17 +0200

    Create linter.sh

---
+++ scripts/linter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Analyzing repository structure..."
+# A real demon will see that they can modify this file in their PR 
+# to 'cat' the environment variables or send them to a webhook.
+echo "No linting errors found. 200 OK."
